5636c81bc3ee59f29bedc0ad6e0877a579791cbe
JorensM <jorensmerenjanu@gmail.com>
2023-12-18 13:48:09 +0200

    clean up and remove main tab feature
    I figured that main tab feature should be a separate package as it is not necessary for tab syncing

--- src/useTabState.tsx
+++ src/useTabState.tsx
@@ -1,181 +1,37 @@
-import React, { useEffect, useMemo, useState, useRef } from 'react';
-import { v4 as uuid } from 'uuid'
+import { useEffect, useMemo, useState } from 'react';
 
-//console.log('creating worker')
 const worker = new SharedWorker(new URL('./worker.js', import.meta.url))
 
-function useTabStateUtil(sync_main_tab?: number) {
+export default function useTabState<T>(initial_state: T, id: string){
+    const [localState, setLocalState] = useState<T>(initial_state);
 
-    const tabId = useMemo(() => {
-        if (!sessionStorage.getItem('tab_id')) {
-            sessionStorage.setItem('tab_id', uuid())
-        }
-        //console.log('this tab: ', sessionStorage.getItem('tab_id'));
-        return sessionStorage.getItem('tab_id')
-    }, [])
-
-    const [isMainTab, setIsMainTab] = useState<boolean>(false)
-
-    const _setIsMainTab = (value: boolean) => {
-        sessionStorage.setItem('is_main', value.toString())
-        setIsMainTab(value)
-    }
-
-    const getIsMainTab = () => {
-        if(sessionStorage.getItem('is_main') === null){
-            _setIsMainTab(false)
-        }
-        return sessionStorage.getItem('is_main') === 'false' ? false : true
-    }
-
-    useEffect(() => {
-
-        worker.port.addEventListener('message', e => {
-            console.log(e.data)
-            if (!e.data?.type) {
-                return
-            }
-            if (e.data.type === 'set_main_tab') {
-                //console.log('in app: setting main tab to ', e.data.tab_id)
-                if (e.data.tab_id === tabId) {
-                    //console.log('this is the main tab')
-                    //debugger
-                    _setIsMainTab(true)
-                } else {
-                    _setIsMainTab(false)
-                }
-            }
-
-            if (e.data.type == 'get_main_tab') {
-                if (e.data.main_tab == tabId) {
-                    _setIsMainTab(true)
-                }
-            }
-        })
-
-        if (sync_main_tab) {
-            setInterval(() => {
-                const is_main = getIsMainTab();
-                console.log('is main:', is_main)
-                setIsMainTab(is_main)
-            }, sync_main_tab)
-        }
-    }, [])
-
-    return { 
-        isMainTab,
-        tabId
-    }
-}
-
-export { useTabStateUtil }
-
-/**
- * Hook that allows you to sync state between tabs
- * 
- * @param {any}         initial_value   Initial value of state. Will be 
- * overwritten by state from other tabs if there are any.
- * @param {string}      id              Unique id to identify the state
- * @param {boolean}     [persist=false] Whether to persist the state even after 
- * all tabs have been closed
- * @param {number}      [timeout]       Timeout after which state will be reset 
- * to initial state. Optional
- * @param {function}    [afterTimeout]  Function to run after timeout has been
- * reached  
- * 
- * @returns [state, setState] array in the same format as a useState() hook
- */
-export default function useTabState<T>(
-    initial_value: T, 
-    id: string, 
-    persist: boolean = false, 
-    timeout: number = null,
-    afterTimeout?: any
-) {
-
-    const _initial_value = useMemo(() => {
-        
-        const local_storage_str = localStorage.getItem(id)
-
-        if (persist && local_storage_str !== null && local_storage_str !== 'undefined') {
-            console.log(local_storage_str)
-            return JSON.parse(local_storage_str)
-        } else {
-            return initial_value
-        }
-
-    }, [])
-
-    const [state, setState] = useState<T>(_initial_value)
-
-    const { isMainTab, tabId } = useTabStateUtil(500)
-
-    const _setState = (new_state: T) => {
-        setState(new_state)
+    const setTabState = (new_state: T) => {
+        setLocalState(new_state)
         worker.port.postMessage({
             type: 'set_state',
-            from_tab: tabId,
             id: id,
             state: new_state
         })
     }
 
     useEffect(() => {
-
-        if (timeout) {
-            setInterval(() => {
-                _setState(initial_value)
-                afterTimeout()
-            }, timeout)
-        }
-
         worker.port.addEventListener('message', e => {
             if (!e.data?.type) {
                 return
             }
 
-            switch (e.data.type) {
-                case 'get_tabs': {
-                    worker.port.postMessage({
-                        type: 'share_tab_id',
-                        tab_id: tabId
-                    })
-                }
+            switch (e.data.type){
                 case 'set_state': {
-                    if (e.data.id == id && e.data.from_tab != tabId) {
-                        setState(e.data.state)
-                    }
-                }
-                case 'get': {
-                    if (e.data.from_tab === tabId && e.data.id === id) {
-                        setState(e.data.state)
+                    if (e.data.id == id) {
+                        setLocalState(e.data.state)
                     }
                 }
             }
-            
         })
-
-        worker.port.start()
-
+        worker.port.start() // This is important, otherwise the messages won't be received.
         worker.port.postMessage({
-            type: 'get',
-            from_tab: tabId,
+            type: 'get_state',
             id: id
         })
-
-        worker.port.postMessage({
-            type: 'get_main_tab'
-        })
     }, [])
-
-    useEffect(() => {
-        if (persist) {
-            localStorage.setItem(id, JSON.stringify(state))
-        }
-    }, [state])
-
-    return [
-        state,
-        _setState
-    ] as const
-}
+  }
--- src/worker.js
+++ src/worker.js
@@ -1,124 +1,41 @@
-const ports = []
-let all_tabs = new Set()
-let main_tab = ''
-// let all_tabs_temp = []
-let check_tabs = false
-let latest_state = {}
+const ports = [] // All ports/tabs
+let latest_state = {} // Up-to-date state
 
-const postMessage = (msg) => {
+// Utility function to send message to all ports at once
+const postMessage = (msg, excluded_port = null) => {
     ports.forEach( port => {
-        port.postMessage(msg)
+        // Don't post message to the excluded port, if one has been specified
+        if(port == excluded_port){
+              return;
+        }
+        port.postMessage(msg);
     })
 }
 
 onconnect = (e) => {
-    postMessage('connected: ', e)
     const port = e.ports[0]
     ports.push(port)
 
-    postMessage({
-        type: 'test'
-    })
-
-    //postMessage(ports)
-
-    
-    
-    const getTabs = () => {
-        check_tabs = false
-        all_tabs.clear()
-        // all_tabs_temp = []
-        postMessage({
-            type: 'get_tabs'
-        })
-        setTimeout(() => {
-            //all_tabs = all
-            setMainTab()
-            check_tabs = true
-        }, 3000)
-    }
-
-    const setMainTab = () => {
-        //postMessage('trying to set main tab')
-        //postMessage(all_tabs)
-        //postMessage(all_tabs)
-        if (all_tabs.size > 0 && !all_tabs.has(main_tab)) {
-            
-            main_tab = Array.from(all_tabs)[0]
-            //postMessage('worker: setting main tab to ' + main_tab)
-        }
-
-        postMessage({
-            type: 'set_main_tab',
-            tab_id: main_tab
-        })
-    }
-
-    getTabs()
-
-    setInterval(() => {
-        if(check_tabs) {
-            getTabs()
-        }
-       
-    }, 500)
-
-    // setInterval(() => {
-    //     // port.postMessage({
-    //     //     msg: "checking if all tabs include main tab",
-    //     //     all_tabs: all_tabs,
-    //     //     main_tab: main_tab
-    //     // })
-
-    //     if (!all_tabs.includes(main_tab)) {
-    //         //port.postMessage('don\'t include')
-    //         setMainTab()
-    //     }
-    // }, 4000)
-
     port.onmessage = (e) => {
-        if (e.data.type === 'share_tab_id') {
-            //postMessage('tab id share: ' + e.data.tab_id)
-            // if (!all_tabs.includes[e.data.tab_id]) {
-            //     all_tabs.push(e.data.tab_id)
-            // }
-            all_tabs.add(e.data.tab_id)
-        }
+        // Sent by a tab to update state in other tabs
         if (e.data.type == 'set_state') {
-            
-            
             latest_state[e.data.id] = e.data.state
 
-            postMessage('setting latest state to')
-            postMessage(latest_state)
-            
+            // Send message to all ports except the requesting one
             postMessage({
                 type: 'set_state',
+                id: e.data.id
+                state: latest_state
+            }, port);
+        }
+        // Sent by a tab to request the value of current state. Used when initializing the state.
+        if (e.data.type == 'get_state') {
+            // Send message ONLY to requesting port
+            port.postMessage({
+                type: 'set_state',
                 id: e.data.id,
-                from_tab: e.data.from_tab,
-                state: e.data.state,
-                test: 'abc'
+                state: latest_state
             })
-        }
-        if (e.data.type == 'get_main_tab') {
-            postMessage({
-                type: 'get_main_tab',
-                from_tab: e.data.from_tab,
-                main_tab: main_tab
-            })
-        }
-        if (e.data.type == 'get') {
-            postMessage('sending latest state for' + e.data.id + ', which is')
-            postMessage(latest_state)
-            if (latest_state[e.data.id] !== undefined) {
-                postMessage({
-                    type: 'get',
-                    id: e.data.id,
-                    from_tab: e.data.from_tab,
-                    state: latest_state[e.data.id]
-                })
-            }
-            
         }
     }
 }
